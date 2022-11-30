7d02a3393ebfe72aecb6179f686bfcef66564d16
Your Name <you@example.com>
2022-11-30 11:14:13 -0500

    lambda-http-func-changereport

--- lambda/mytest.js
+++ lambda/mytest.js
@@ -6,16 +6,55 @@ const tryThisFunction = async () => {
   let commits = await response.json();
   console.log("result", commits[0].author.login);
 }; */
-const tryWithHttps = async () => {
+const tryWithHttps = async (thisToken) => {
   return new Promise((resolve, reject) => {
     const data = JSON.stringify({
-      name: "Deven Rathore",
-      age: 21,
+      event: {
+        header: {
+          namespace: "Alexa",
+          name: "ChangeReport",
+          messageId: "1bd5d003-31b9-476f-ad03-71d471922820",
+          payloadVersion: "3",
+        },
+        endpoint: {
+          scope: {
+            type: "BearerToken",
+            token: `${thisToken}`,
+          },
+          endpointId: "sample-switch-01",
+          cookie: {},
+        },
+        payload: {
+          change: {
+            cause: {
+              type: "APP_INTERACTION",
+            },
+            properties: [
+              {
+                namespace: "Alexa.PowerController",
+                name: "powerState",
+                value: "OFF",
+                timeOfSample: "2022-11-20T16:20:50Z",
+                uncertaintyInMilliseconds: 60000,
+              },
+            ],
+          },
+        },
+      },
+      context: {
+        namespace: "Alexa.EndpointHealth",
+        name: "connectivity",
+        value: {
+          value: "OK",
+        },
+        timeOfSample: "2021-06-03T16:00:50Z ",
+        uncertaintyInMilliseconds: 0,
+      },
     });
 
     const options = {
-      hostname: "reqres.in",
-      path: "/api/users",
+      hostname: "api.amazonalexa.com",
+      path: "/v3/events",
       method: "POST",
       headers: {
         "Content-Type": "application/json",
