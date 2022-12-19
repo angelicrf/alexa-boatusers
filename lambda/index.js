0da909a733ad20d58908d480449258984609b66b
Your Name <you@example.com>
2022-12-19 13:15:37 -0500

    alexa-change-report-event

--- lambda/index.js
+++ lambda/index.js
@@ -53,7 +53,41 @@ const getUserProfileInfo = async (thisToken) => {
 };
 const sendChangeReportEvent = async (thisToken) => {
   return new Promise((resolve, reject) => {
-    const data = JSON.stringify({
+    let options = {
+      method: "POST",
+      hostname: "api.amazonalexa.com",
+      path: "/v3/events",
+      headers: {
+        "Content-Type": "application/json;charset=UTF-8",
+        Authorization: `Bearer ${thisToken}`,
+      },
+    };
+
+    var req = myRequest.request(options, function (res) {
+      let getDate = new Date().toISOString();
+      var chunks = [];
+
+      res.on("data", function (chunk) {
+        chunks.push(chunk);
+      });
+
+      res.on("end", function (chunk) {
+        log(
+          "DEBUG Angelique Boat Users Send ",
+          "Change Report",
+          "Befor Body Concat"
+        );
+        var body = Buffer.concat(chunks);
+        console.log(`If Body Not Empty ${body.toString()}`);
+        resolve("Sent ChangeReport Event");
+      });
+
+      res.on("error", function (error) {
+        console.error(error);
+        reject(error);
+      });
+    });
+    var postData = JSON.stringify({
       event: {
         header: {
           namespace: "Alexa",
@@ -67,7 +101,6 @@ const sendChangeReportEvent = async (thisToken) => {
             token: `${thisToken}`,
           },
           endpointId: "sample-switch-01",
-          cookie: {},
         },
         payload: {
           change: {
@@ -79,8 +112,8 @@ const sendChangeReportEvent = async (thisToken) => {
                 namespace: "Alexa.PowerController",
                 name: "powerState",
                 value: "OFF",
-                timeOfSample: "2022-11-20T16:20:50Z",
-                uncertaintyInMilliseconds: 500,
+                timeOfSample: `${getDate}`,
+                uncertaintyInMilliseconds: 0,
               },
               {
                 namespace: "Alexa.EndpointHealth",
@@ -88,55 +121,17 @@ const sendChangeReportEvent = async (thisToken) => {
                 value: {
                   value: "OK",
                 },
-                timeOfSample: "2021-06-03T16:00:50Z ",
+                timeOfSample: `${getDate}`,
                 uncertaintyInMilliseconds: 0,
               },
             ],
           },
         },
       },
-      context: {
-        namespace: "Alexa.EndpointHealth",
-        name: "connectivity",
-        value: {
-          value: "OK",
-        },
-        timeOfSample: "2021-06-03T16:00:50Z ",
-        uncertaintyInMilliseconds: 0,
-      },
+      context: {},
     });
+    req.write(postData);
 
-    const options = {
-      hostname: "api.amazonalexa.com",
-      path: "/v3/events",
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-        "Content-Length": data.length,
-      },
-    };
-
-    const req = myRequest
-      .request(options, (res) => {
-        let data = "";
-
-        console.log("Status Code:", res.statusCode);
-
-        res.on("data", (chunk) => {
-          data += chunk;
-        });
-
-        res.on("end", () => {
-          console.log("Body: ", JSON.parse(data));
-          return resolve(JSON.parse(data));
-        });
-      })
-      .on("error", (err) => {
-        console.log("Error: ", err.message);
-        return reject(err.message);
-      });
-
-    req.write(data);
     req.end();
   });
 };
@@ -204,13 +199,67 @@ const handleStateReport = async (request, context) => {
     "Alexa.PowerController ",
     JSON.stringify(response)
   );
-  //await sendChangeReportEvent(response.event.scope.token);
+
   await getUserProfileInfo(requestToken);
   context.succeed(response);
 };
 
-const handleAuthorization = (request, context) => {
-  // Send the AcceptGrant response
+const issueAccessToken = (thisCode) => {
+  return new Promise((resolve, reject) => {
+    var options = {
+      method: "POST",
+      hostname: "api.amazon.com",
+      path: "/auth/o2/token",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    };
+
+    var req = myRequest.request(options, (res) => {
+      let data = "";
+
+      res.on("data", (chunk) => {
+        data += chunk;
+      });
+
+      res.on("end", () => {
+        let responseAToken = JSON.parse(data).access_token;
+        console.log(responseAToken);
+        resolve(responseAToken);
+      });
+
+      res.on("error", (error) => {
+        console.error(error);
+        reject(error);
+      });
+    });
+
+    let postData = JSON.stringify({
+      grant_type: "authorization_code",
+      code: `${thisCode}`,
+      client_id:
+        "amzn1.application-oa2-client.45e216f2fc214eec95a17fd39a95146c",
+      client_secret:
+        "0edaa57cd2a556c1a5c8c30e5f5e710a620c39a027d24cedc7c8624c45afa726",
+      redirect_uri:
+        "https://skills-store.amazon.com/api/skill/link/M2FC9FL6SREJQF",
+    });
+
+    req.write(postData);
+
+    req.end();
+  });
+};
+
+const handleAuthorization = async (request, context) => {
+  log("DEBUG", "GetCode Response: ", request.directive.payload.grant.code);
+  let getAccessToken = await issueAccessToken(
+    request.directive.payload.grant.code
+  );
+  log("DEBUG", "GetAccessToken Response: ", getAccessToken);
+  // Send Event Change Report
+  await sendChangeReportEvent(getAccessToken);
+  // store accessToken to db to send other events
   var payload = {};
   var header = request.directive.header;
   header.name = "AcceptGrant.Response";
@@ -269,22 +318,7 @@ const handleChangeReport = (request, context) => {
         payload: {},
       },
 
-      //payload: {
-      //   change: {
-      //     cause: {
-      //       type: "PHYSICAL_INTERACTION"
-      //      },
-      //      properties: [
-      //       {
-      //         "namespace": "Alexa.PowerController",
-      //         "name": "powerState",
-      //         "value": "ON",
-      //         "timeOfSample": "2022-11-20T16:20:50.52Z", //retrieve from result.
-      //         "uncertaintyInMilliseconds": 50
-      //       }
-      //      ]
-      //   }
-      //},
+      //payload: {},
     },
   };
   log(
