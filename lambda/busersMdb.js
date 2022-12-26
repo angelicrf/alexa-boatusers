ed974ea4066ee901a24214cd0b0313a09dac9548
Your Name <you@example.com>
2022-12-26 12:07:58 -0500

    get-insert-update-buMdb

--- lambda/busersMdb.js
+++ lambda/busersMdb.js
@@ -1,25 +1,84 @@
 const mdbUrl =
   "mongodb+srv://boatusersmdb:busers123456@budatabase.larhnar.mongodb.net/budb?retryWrites=true&w=majority";
 const { MongoClient } = require("mongodb");
+var ObjectId = require("mongodb").ObjectId;
 
 const uri = mdbUrl;
 
 const client = new MongoClient(uri);
+const database = client.db("budb");
+const buData = database.collection("bucollection");
 
-const buMdbFindData = async () => {
-  try {
-    const database = client.db("budb");
-    const buData = database.collection("bucollection");
-
-    const query = { title: "buTitle" };
-    const buFindData = await buData.findOne(query);
-
-    console.log(buFindData);
-  } catch (err) {
-    console.log(err);
-  } finally {
-    await client.close();
-  }
+const buMdbFindData = () => {
+  return new Promise(async (resolve, reject) => {
+    try {
+      const query = {
+        title: "a new Bu Tile: mercerIsland 0.5590648127577209",
+      };
+      const buFindData = await buData.findOne(query);
+      let getResult = buFindData._id;
+      console.log(getResult);
+      resolve(getResult);
+    } catch (err) {
+      console.log(err);
+      reject(err);
+    }
+    // finally {
+    // await client.close();
+    //}
+  });
 };
+const buMdbInsertData = (thisTitle, thisContent) => {
+  return new Promise(async (resolve, reject) => {
+    try {
+      let doc = {
+        title: `${thisTitle}`,
+        content: `${thisContent}`,
+      };
+      const result = await buData.insertOne(doc);
+      console.log(
+        `A Bu new Data was inserted with the _id: ${result.insertedId}`
+      );
 
-buMdbFindData();
+      resolve(result.insertedId);
+    } catch (err) {
+      console.log(err);
+      reject(err);
+    }
+    // finally {
+    // await client.close();
+    //}
+  });
+};
+const buMdbUpdataData = (thisId, thisTitle) => {
+  return new Promise(async (resolve, reject) => {
+    try {
+      const filter = { _id: ObjectId(`${thisId}`) };
+      const options = { upsert: true };
+      const updateDoc = {
+        $set: {
+          title: `a new Bu Tile: ${thisTitle} ${Math.random()}`,
+        },
+      };
+      const result = await buData.updateOne(filter, updateDoc, options);
+      console.log(
+        `${result.matchedCount} document(s) matched the filter, updated ${result.modifiedCount} document(s)`
+      );
+      resolve(result.modifiedCount);
+    } catch (err) {
+      console.log(err);
+      reject(err);
+    } finally {
+      await client.close();
+    }
+  });
+};
+const runMdbProcess = async () => {
+  let getId = await buMdbFindData();
+  console.log(`getData is : ${getId}`);
+  //let addData = await buMdbInsertData("angeliqueBU", "buDataInfo");
+  //console.log(`dataAdded ${addData}`);
+  let updateData = await buMdbUpdataData(getId, "orcaIsland");
+  console.log(`updatedData ${updateData}`);
+};
+runMdbProcess();
