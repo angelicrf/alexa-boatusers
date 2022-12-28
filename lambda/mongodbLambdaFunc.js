6d315b8ef7aab477a7b4dd834e6cc5d2f266f736
Your Name <you@example.com>
2022-12-28 12:34:50 -0500

    aws-sts-aaume-role

--- lambda/mongodbLambdaFunc.js
+++ lambda/mongodbLambdaFunc.js
@@ -1,23 +1,31 @@
 const { MongoClient } = require("mongodb");
 //cmd : aws sts get-session-token
-// replace values with process.env
-const mongoUrl =
-  "mongodb+srv://AKIAUWH7XUSKRJVKKJHU:bVJUeJri2SraO8/EqVc2LjfJuzYvNzvpRLpaZZpv@budatabase.larhnar.mongodb.net/?authSource=%24external&authMechanism=MONGODB-AWS&retryWrites=true&w=majority&authMechanismProperties=AWS_SESSION_TOKEN:IQoJb3JpZ2luX2VjENT//////////wEaCXVzLWVhc3QtMSJGMEQCIEbffm74/+eZy0ccB7fKIXDblTuhpnfARNLJ1dwZ+/w7AiANb1mPUuL2vHDY2KcGB/lf/S5gZ77vOeDK/qpk/rttZirrAQgdEAQaDDMyMjY1ODg3MDQyMSIMf+CPXRTrQJwyN+wDKsgBH3SNNv1C8zeX/Wg+pXReYKtWEot23I0IhdMs9w884sGRBJehpeh/A+Ouob4z829z+BjEs+6hcMMGyqnxFQ3A/otyC2snDXCrUIeqwICMeBKDqTrU2Rf03ZCSWQKpSOST0afWuYn/5E5ZpmYYzTP8QHCRQPMuaJbbHgLNRL6U2sgvy08O8aA3FcSzTjkbQCzQ35Y+2521OZGBezkBkFz2gH+OSSCvLdCFPXB2uRPNIw2CJGtcZnI6GrK1zRbVKw5zvG1V2yVMFoQwtO2nnQY6mQF7S/cx+K55YtGtDSM7RljkDepgwCzqgkEcjjDnSVncn/IEkDuQmvF6M3kyD0lYq4CYmKfPIQfbFrtzSjC5uAckLkRx1xkDSMx1AKX/DxGuUsU0lyAVMXTar6QLkBJHJUNDYO3q6oH8SQ6berixqbvPw+uGFbreIzubs4O9aUHzOCvsRDP6jTB3ekn6maWFJ8/5y+K79GW7AQ8=";
-const client = new MongoClient(mongoUrl);
-const database = client.db("budb");
-const buData = database.collection("bucollection");
-/* const client = new MongoClient(process.env.MONGODB_URI, {
-  auth: {
-    username: "AKIAUWH7XUSKRJVKKJHU",
-    password: "bVJUeJri2SraO8/EqVc2LjfJuzYvNzvpRLpaZZpv",
-  },
-  authSource: "$external",
-  authMechanism: "MONGODB-AWS",
-}); */
+//cmd atlas cloudProviders accessRoles aws create --projectId 63a9c2c0a6942d034b8beef6
+//make sure to add conditions to IAM specific role's trust relationships before authorizing arn
+//"Condition": {
+//  "StringEquals": {
+//    "sts:ExternalId": "3f8669c2-8b51-4868-bfde-778d246e8631"
+//  }
+//}
+//cmd atlas cloudProviders accessRoles aws authorize arn:aws:iam::322658870421:role/lambda_exec_BoatUsersLmbdFunc --projectId 63a9c2c0a6942d034b8beef6
+//remove the conditions and save IAM role before running sts assume-role
+//aws sts assume-role --role-arn arn:aws:iam::322658870421:role/boatusers-mdb-cloudproviders-role --role-session-name "RoleSession2" --profile boatusers
+//replace values with process.env
+
+var username = encodeURIComponent("ASIAUWH7XUSKVL2BKTE5");
+var password = encodeURIComponent("b6AZl5IO+rvM68ioqVt57ETUn+ghJDL/DcOMC8I6");
+var thisSessionToken = encodeURIComponent(
+  "IQoJb3JpZ2luX2VjEAEaCXVzLWVhc3QtMSJHMEUCIQCQm4EWcaawW5nH6f4xta6A5bQbz9v7KZDtPvD75UkMQgIgfIFMHdUR/8K4Gjvu+2Je/rTYrh6kVHZ9fSzq4rUW774qmQIIShAEGgwzMjI2NTg4NzA0MjEiDIXiluWi0m6NWHDKYCr2AYe64xfHiQzLLgRT8e/OfscyoBerZmUe54RtV6m7N8W2rsPoi5ZK4sjFzG+Tod+nj7v4Zm8pW5cjhYUDgvDR5SE2GgmJh3o6UCwBnWW6tnkhQ9HTq7gGHIQ7+lwtkbGUlKpSWJ8FrXMXExvwvX2895SbVLcjxZ0dCFWVSG33Dp8O0WmdMUEVdlhM0YVaOyEL8ihRF8LQxgQTbmhVmss+WPCjHscJmaCK3wP6WazkpAbejHq2Fi65lUKL/n16BDl/okpJAzuQ4hsAJ+4v4nnZFpGIgDWZEARDk6SdpRp6hc4bXRoAUBOnHMTGknQWPB7eIYiBJV+ArzDm6rGdBjqdAWLJqFCZPOHfhnyPHKP2+fuhCAWNHbPkgFTzFMUGZTO42BEHRQW6yLs1DT59ZE/DIpGgdbJzNkJkYBGHTn8OmfdCNjZIH0psCs76ibPYcMbqtBCdBRoJFIvrvwHjPR1BDxKluOEfMECJGb/9o8hOgfSE0MJ6ZQf9YpU8wihZE+ZTLluKcQrynHP6sucm+z0hVlNvAw5RgZsm0L8JKS0="
+);
+const mongoUrl = `mongodb+srv://ASIAUWH7XUSKVL2BKTE5:${password}@budatabase.larhnar.mongodb.net/budb?authSource=%24external&authMechanism=MONGODB-AWS&retryWrites=true&w=majority&authMechanismProperties=AWS_SESSION_TOKEN:${thisSessionToken}`;
 
 const buMdbFindData = () => {
   return new Promise(async (resolve, reject) => {
     try {
+      const client = new MongoClient(mongoUrl);
+
+      let database = client.db("budb");
+      const buData = database.collection("bucollection");
       const query = {
         title: "a new Bu Tile: mercerIsland 0.5590648127577209",
       };
@@ -31,6 +39,7 @@ const buMdbFindData = () => {
     }
   });
 };
+
 module.exports.handler = async () => {
   console.log("module called");
   let getMdbData = await buMdbFindData();
